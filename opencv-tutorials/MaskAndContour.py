6e09e91e2586c85069dab803010854d224503d89
wegscd <Douglas_E_Wegscheid@whirlpool.com>
2017-10-19 14:12:58 -0400

    draw the contours

--- opencv-tutorials/MaskAndContour.py
+++ opencv-tutorials/MaskAndContour.py
@@ -7,6 +7,22 @@ Created on Oct 18, 2017
 import cv2
 import numpy as np
 
+kernel = np.ones((5,5), np.uint8)
+
+# https://sashat.me/2017/01/11/list-of-20-simple-distinct-colors/
+colors = (
+    (230, 25, 75),
+    (60, 180, 75),
+    (255, 225, 25),
+    (0, 130, 200),
+    (245, 130, 48),
+    (145, 30, 180),
+    (70, 240, 240),
+    (240, 50, 230),
+    (210, 245, 60),
+    (250, 190, 190)
+)
+
 cap = cv2.VideoCapture(0)
 
 while cap.isOpened():
@@ -23,9 +39,11 @@ while cap.isOpened():
 
     # Threshold the HSV image to get only blue colors
     mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    mask = cv2.erode(mask, kernel, iterations=1)
+    mask = cv2.dilate(mask, kernel, iterations=1)
 
     # Bitwise-AND mask and original image
-    res = cv2.bitwise_and(frame,frame, mask= mask)
+    res = cv2.bitwise_and(frame, frame, mask= mask)
 
     cv2.imshow('frame',frame)
     cv2.imshow('mask',mask)
@@ -47,10 +65,13 @@ while cap.isOpened():
         xc = x + (w/2)
         yc = y + (h/2)
         
-        image2 = cv2.line(image2,(xc - 20, yc),(xc + 20, yc), (255,0,0), 1)
-        image2 = cv2.line(image2,(xc, yc - 20),(xc, yc + 20), (255,0,0), 1)
+        image2 = cv2.line(image2, (xc - 20, yc), (xc + 20, yc), (255, 0, 0), 1)
+        image2 = cv2.line(image2, (xc, yc - 20), (xc, yc + 20), (255, 0, 0), 1)
         # image2 = cv2.drawContours(image2, [cnt], 0, (0,255,0), 3)
-        # cv2.rectangle(image2,(384,0),(510,128),(0,255,0),3)
+        
+        for i, contour in enumerate(contours):
+            cv2.drawContours(image2, [contour], -1, colors[i % len(colors)], 1)
+        
         cv2.imshow('image2',image2)
     
     #print contours
